66bf52ca952aa7c8d28996a274320939ec3b4bcc
bookinstock2 <wendelu.1991@gamil.com>
2026-02-13 10:19:19 +0800

    feat: 添加乘法函数模块

---
+++ multiply.py
@@ -0,0 +1,11 @@
+def multiply(a, b):
+    """返回两数之积"""
+    return a * b
+
+
+if __name__ == "__main__":
+    # 简单演示
+    num1 = 6
+    num2 = 7
+    result = multiply(num1, num2)
+    print(f"{num1} * {num2} = {result}")
